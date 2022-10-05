b8e68689500078ec291af8505ec7f16c298449a4
Jorge Madrigal <ulisonmadrigal@gmail.com>
2022-10-05 18:37:03 -0500

    PrimerCommit

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+
+    public static void main(String args[]){
+        int div;
+        int edad = 35;
+        int result;
+        div = 2;
+    
+        result = edad/div;
+
+        System.out.println("Resultado: " + result);
+    }
+}
